f186c6ad48aa0bb3be9735cb02a6337ad730a809
dfamiliarstranger <70381661+dfamiliarstranger@users.noreply.github.com>
2022-11-15 15:30:42 +0100

    title change

--- main.js
+++ main.js
@@ -23,24 +23,24 @@ if (document.readyState == "loading") {
 //making function
 function ready() {
   //Remove Items From Cart
-  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  let removeCartButtons = document.getElementsByClassName("cart-remove");
   console.log(removeCartButtons);
-  for (var i = 0; i < removeCartButtons.length; i++) {
-    var button = removeCartButtons[i];
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    let button = removeCartButtons[i];
     button.addEventListener("click", removeCartItem);
   }
 
   //Quantity Change
-  var quantityInputs = document.getElementsByClassName("cart-quantity");
-  for (var i = 0; i < quantityInputs.length; i++) {
-    var input = quantityInputs[i];
+  let quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
     input.addEventListener("change", quantityChanged);
   }
 
   //Add to cart
-  var addCart = document.getElementsByClassName("add-cart");
-  for (var i = 0; i < addCart.length; i++) {
-    var button = addCart[i];
+  let addCart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
     button.addEventListener("click", addCartClicked);
   }
 
@@ -52,24 +52,25 @@ function ready() {
 
 // Buy Button
 function buyButtonClicked() {
-  alert("your order has been placed");
-  var cartContent = document.getElementsByClassName("cart-content")[0];
-  while (cartContent.hasChildNodes()) {
-    cartContent.removeChild(cartContent.firstChild);
-  }
-  updatetotal();
+  payWithPaystack(total_value)
+  // alert("your order has been placed");
+  // let cartContent = document.getElementsByClassName("cart-content")[0];
+  // while (cartContent.hasChildNodes()) {
+  //   cartContent.removeChild(cartContent.firstChild);
+  // }
+  // updatetotal();
 }
 
 //Remove Items from cart
 function removeCartItem(event) {
-  var buttonClicked = event.target;
+  let buttonClicked = event.target;
   buttonClicked.parentElement.remove();
   updatetotal();
 }
 
 //Quantity  changed
 function quantityChanged(event) {
-  var input = event.target;
+  let input = event.target;
   if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
@@ -78,28 +79,28 @@ function quantityChanged(event) {
 
 // Add to cart
 function addCartClicked(event) {
-  var button = event.target;
-  var shopProducts = button.parentElement;
-  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
-  var price = shopProducts.getElementsByClassName("price")[0].innerText;
-  var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  let button = event.target;
+  let shopProducts = button.parentElement;
+  let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  let price = shopProducts.getElementsByClassName("price")[0].innerText;
+  let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
   addProductTocart(title, price, productImg);
   updatetotal();
 }
 
 function addProductTocart(title, price, productImg) {
-  var cartShopBox = document.createElement("div");
+  let cartShopBox = document.createElement("div");
   cartShopBox.classList.add("cart-box");
-  var cartItems = document.getElementsByClassName("cart-content")[0];
-  var cartItemNames = cartItems.getElementsByClassName("cart-product-title");
-  for (var i = 0; i < cartItemNames.length; i++) {
+  let cartItems = document.getElementsByClassName("cart-content")[0];
+  let cartItemNames = cartItems.getElementsByClassName("cart-product-title");
+  for (let i = 0; i < cartItemNames.length; i++) {
     if (cartItemNames[i].innerText == title) {
       alert("This item is already added to the cart");
       return;
     }
   }
 
-  var cartBoxContent = `<img src="${productImg}" alt="" class="cart-img">
+  let cartBoxContent = `<img src="${productImg}" alt="" class="cart-img">
                               <div class="detail-box">
                                   <div class="cart-product-title"> ${title} </div>
                                   <div class="cart-price">${price}</div>
@@ -118,21 +119,51 @@ function addProductTocart(title, price, productImg) {
     .addEventListener("change", quantityChanged);
 }
 
+let total_value = 0;
 //Update Total
 function updatetotal() {
-  var cartContent = document.getElementsByClassName("cart-content")[0];
-  var cartBoxes = cartContent.getElementsByClassName("cart-box");
-  var total = 0;
-  for (var i = 0; i < cartBoxes.length; i++) {
-    var cartBox = cartBoxes[i];
-    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-    var price = parseFloat(priceElement.innerText.replace("$", ""));
-    var quantity = quantityElement.value;
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+  let total_quantity = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let quantity = quantityElement.value;
+    total_quantity += parseInt(quantity);
     total = total + price * quantity;
   }
     //if price contains some decimal
     total = Math.round(total * 100) / 100;
     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
   
+    document.getElementsByClassName("cart-counter")[0].innerText = total_quantity;
+    total_value = parseInt(total);
+}
+
+
+// paystack
+
+const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+function payWithPaystack(total) {
+  let handler = PaystackPop.setup({
+    key: 'pk_live_2ac881cf198548a844e88ea6999363c9c6c0696d', // Replace with your public key
+    email: "eniola.o@gmail.com",
+    amount: total * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
 }
